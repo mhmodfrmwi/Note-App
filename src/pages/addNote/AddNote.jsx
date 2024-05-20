ebef37da08fa4007538056ba07a7424513d3a8ad
mhmodfrmwi <mahmoudframawi98@gmail.com>
2024-05-20 12:13:27 +0300

    fix add note responsive

--- src/pages/addNote/AddNote.jsx
+++ src/pages/addNote/AddNote.jsx
@@ -5,26 +5,28 @@ import { saveToDatabase } from "../../lib/utils/saveToDataBase";
 import fetchFromDatabase from "../../lib/utils/fetchFromDatabase";
 import { useDispatch } from "react-redux";
 import { addNote } from "../../rtk/slices/notesSlice";
+
 function AddNote() {
   const [noteTitle, setNoteTitle] = useState("");
   const [noteAuthor, setNoteAuthor] = useState("");
   const [noteContent, setNoteContent] = useState("");
   const dispatch = useDispatch();
+
   const handleTitle = (e) => {
     e.preventDefault();
     setNoteTitle(e.target.value);
-    console.log(noteTitle);
   };
+
   const handleAuthor = (e) => {
     e.preventDefault();
     setNoteAuthor(e.target.value);
-    console.log(noteAuthor);
   };
+
   const handleContent = (e) => {
     e.preventDefault();
     setNoteContent(e.target.value);
-    console.log(noteContent);
   };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const buttonName = e.nativeEvent.submitter.name;
@@ -41,7 +43,7 @@ function AddNote() {
     if (buttonName === "add_note") {
       note.pinned = false;
     } else {
-      Notes.map((note) => {
+      Notes.forEach((note) => {
         if (note.pinned === true) {
           note.pinned = false;
         }
@@ -50,26 +52,20 @@ function AddNote() {
     }
     Notes.push(note);
     dispatch(addNote(note));
-    console.log(Notes);
     saveToDatabase("notes", Notes);
   };
+
   return (
     <div className="addNoteContainer">
       <Sidebar />
       <form onSubmit={handleSubmit} className="addnote">
         <h1>Add Note</h1>
-        <label>title</label>
-        <input id="title" type="textarea" required onChange={handleTitle} />
-        <label>Author</label>
-        <input id="author" type="textarea" required onChange={handleAuthor} />
-        <label>Your Note</label>
-        <textarea
-          id="note"
-          name="w3review"
-          rows="4"
-          cols="50"
-          onChange={handleContent}
-        />
+        <label htmlFor="title">Title</label>
+        <input id="title" type="text" required onChange={handleTitle} />
+        <label htmlFor="author">Author</label>
+        <input id="author" type="text" required onChange={handleAuthor} />
+        <label htmlFor="note">Your Note</label>
+        <textarea id="note" rows="4" required onChange={handleContent} />
         <div className="buttons__container">
           <button type="submit" name="add_note" className="add_note">
             Add Note
@@ -82,4 +78,5 @@ function AddNote() {
     </div>
   );
 }
+
 export default AddNote;
